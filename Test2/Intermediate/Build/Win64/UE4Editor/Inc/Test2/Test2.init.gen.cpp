155b797209124edbb6740642731816212f7c4cf5
EmilianoP2310 <emiliano@anunciatedigital.com>
2020-07-17 13:25:25 -0500

    BallBound class

--- Test2/Intermediate/Build/Win64/UE4Editor/Inc/Test2/Test2.init.gen.cpp
+++ Test2/Intermediate/Build/Win64/UE4Editor/Inc/Test2/Test2.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeTest2_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xDBD7EFCD,
-				0x92B2BDEC,
+				0xDA9E0DCF,
+				0x16078037,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
